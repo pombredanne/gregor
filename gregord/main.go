3e97963998eaf92f77be82c0ee97cf253ffd3573
Patrick Crosby <pcrosby@gmail.com>
2016-04-01 16:08:01 -0500

    Add dummy authenticator to gregord server

--- gregord/main.go
+++ gregord/main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"os"
 
+	protocol "github.com/keybase/gregor/protocol/go"
 	"github.com/keybase/gregor/rpc"
+	"golang.org/x/net/context"
 )
+
+type dummyauth struct{}
+
+func (m dummyauth) Authenticate(_ context.Context, tok protocol.AuthToken) (protocol.UID, protocol.SessionID, error) {
+	return protocol.UID{}, protocol.SessionID(""), nil
+}
 
 func main() {
 	opts, err := ParseOptions(os.Args)
@@ -12,7 +20,9 @@ func main() {
 		errorf("%s\n", err)
 		os.Exit(2)
 	}
-	err = newMainServer(opts, rpc.NewServer()).listenAndServe()
+	srv := rpc.NewServer()
+	srv.SetAuthenticator(&dummyauth{})
+	err = newMainServer(opts, srv).listenAndServe()
 	if err != nil {
 		errorf("%s\n", err)
 		os.Exit(2)
