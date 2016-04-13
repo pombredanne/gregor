b77e836e7bb1115c3939d8fa6fca3a149f21b45d
Jacob H. Haven <jacob@jhaven.me>
2016-04-13 15:05:05 -0700

    Add TestHashState testing Hash on shuffled state orders

--- protocol/gregor1/serialize_test.go
+++ protocol/gregor1/serialize_test.go
@@ -1,14 +1,17 @@
 package gregor1
 
 import (
+	"bytes"
+	"math/rand"
 	"reflect"
 	"testing"
 	"time"
 
+	"github.com/jonboulle/clockwork"
 	gregor "github.com/keybase/gregor"
 )
 
-func MakeTestState(o gregor.ObjFactory) (gregor.State, error) {
+func MakeTestItems(o gregor.ObjFactory, fc clockwork.FakeClock) ([]gregor.Item, error) {
 	u, err := o.MakeUID([]byte{0x01, 0x02, 0x03})
 	if err != nil {
 		return nil, err
@@ -31,21 +34,36 @@ func MakeTestState(o gregor.ObjFactory) (gregor.State, error) {
 
 	items := make([]gregor.Item, 12)
 	for i := range items {
-		msgid, err := o.MakeMsgID([]byte{byte(i)})
+		var b [8]byte
+		if _, err := rand.Read(b[:]); err != nil {
+			return nil, err
+		}
+
+		msgid, err := o.MakeMsgID(b[:])
 		if err != nil {
 			return nil, err
 		}
-		items[i], err = o.MakeItem(u, msgid, deviceid, time.Now(), c, nil, body)
+
+		if i%3 == 0 {
+			fc.Advance(time.Second)
+		}
+		items[i], err = o.MakeItem(u, msgid, deviceid, fc.Now(), c, nil, body)
 		if err != nil {
 			return nil, err
 		}
 	}
-	return o.MakeState(items)
+	return items, nil
 }
 
 func TestMarshalState(t *testing.T) {
 	var o ObjFactory
-	state, err := MakeTestState(o)
+	fc := clockwork.NewFakeClock()
+	items, err := MakeTestItems(o, fc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	state, err := o.MakeState(items)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -62,5 +80,44 @@ func TestMarshalState(t *testing.T) {
 
 	if !reflect.DeepEqual(state, state2) {
 		t.Fatal("states not equal")
+	}
+}
+
+func TestHashState(t *testing.T) {
+	var o ObjFactory
+	fc := clockwork.NewFakeClock()
+	items, err := MakeTestItems(o, fc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	perm := rand.Perm(len(items))
+	shuffled := make([]gregor.Item, len(items))
+	for i := range shuffled {
+		shuffled[i] = items[perm[i]]
+	}
+
+	state1, err := o.MakeState(items)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	state2, err := o.MakeState(shuffled)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h1, err := state1.Hash()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h2, err := state2.Hash()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(h1, h2) {
+		t.Fatal("shuffled state hash not equal to original")
 	}
 }
