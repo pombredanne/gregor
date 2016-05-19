7494a816dd892d4c42f73e75da76eb9f22c7bf97
Jacob H. Haven <jacob@jhaven.me>
2016-05-19 14:59:13 -0700

    Add scare warning to dbinit (#85)

--- bin/dbinit/dbinit.go
+++ bin/dbinit/dbinit.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/keybase/gregor/bin"
@@ -11,12 +14,14 @@ import (
 )
 
 var (
-	dsnVar   = "MYSQL_DSN"
-	s3conf   bin.S3Config
-	mysqlDSN = &bin.DSNGetter{S: os.Getenv(dsnVar), S3conf: &s3conf}
+	skipCheck bool
+	dsnVar    = "MYSQL_DSN"
+	s3conf    bin.S3Config
+	mysqlDSN  = &bin.DSNGetter{S: os.Getenv(dsnVar), S3conf: &s3conf}
 )
 
 func init() {
+	flag.BoolVar(&skipCheck, "y", false, "skip confirmation and init db")
 	flag.StringVar(&s3conf.AWSRegion, "aws-region", os.Getenv("AWS_REGION"), "AWS region if running on AWS")
 	flag.StringVar(&s3conf.ConfigBucket, "s3-config-bucket", os.Getenv("S3_CONFIG_BUCKET"), "where our S3 configs are stored")
 	flag.Var(mysqlDSN, "mysql-dsn", "user:pw@host/dbname for MySQL")
@@ -38,6 +43,27 @@ func main() {
 		dsn = v
 	default:
 		log.Fatal("Error parsing mysql DSN")
+	}
+
+	if !skipCheck {
+		fmt.Println("Creating/Reseting DB at", dsn)
+		fmt.Println("This will lead to irreversible loss of data currently in the DB.")
+		fmt.Print("Are you sure? (Y/N): ")
+		reader := bufio.NewReader(os.Stdin)
+		resp, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp = strings.TrimSuffix(strings.ToUpper(resp), "\n")
+		switch resp {
+		case "Y":
+			fmt.Println("Creating DB...")
+		case "N":
+			log.Fatal("Chose not to create DB.")
+		default:
+			log.Fatal("Invalid response")
+		}
 	}
 
 	if _, err := storage.CreateDB("mysql", dsn); err != nil {
