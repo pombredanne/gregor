61902bfe9c2ede74ede57ffe42db5bd8877eecd5
Maxwell Krohn <themax@gmail.com>
2016-05-19 13:01:42 -0400

    Make errors human readable
    before:
     ERRO 12:57:35.218 main.go:74 &net.OpError{Op:"listen", Net:"tcp", Source:net.Addr(nil), Addr:(*net.TCPAddr)(0xc820016240), Err:(*os.SyscallError)(0xc82000e280)}
    
    after:
     ERRO 13:00:16.334 main.go:74 listen tcp 127.0.0.1:9911: bind: address already in use

--- bin/rsenderd/main.go
+++ bin/rsenderd/main.go
@@ -15,20 +15,20 @@ func main() {
 
 	opts, err := ParseOptions(os.Args)
 	if err != nil {
-		log.Error("%#v", err)
+		log.Error("%s", err)
 		os.Exit(1)
 	}
 
 	db, err := sql.Open("mysql", opts.MysqlDSN)
 	if err != nil {
-		log.Error("%#v", err)
+		log.Error("%s", err)
 		os.Exit(2)
 	}
 
 	r := newRSender(db, opts.RemindServer, log)
 	for _ = range time.Tick(opts.RemindDuration) {
 		if err := r.sendReminders(); err != nil {
-			log.Error("%#v", err)
+			log.Error("%s", err)
 		}
 	}
 }
